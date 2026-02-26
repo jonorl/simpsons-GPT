29bc9ebb18bfb662ee6be53a3955591428d7dfe5
jonorl <39122230+jonorl@users.noreply.github.com>
2026-02-26 19:38:06 +0300

    Fix: both legacy and new route works now

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/simpsons-gpt/',
+    base: './',
   plugins: [react(), tailwindcss()],
 })
